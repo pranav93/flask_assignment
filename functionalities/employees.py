85272856e4fee9d42b96323a1b036a9d3d9b0339
pranav <ambhore.pranav@gmail.com>
2019-12-18 15:51:25 +0000

    added logger

--- functionalities/employees.py
+++ functionalities/employees.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from sqlalchemy import distinct
 
 from functionalities.employee_gifts import EmployeeGift
@@ -39,17 +40,25 @@ class Employee(object):
         gift_category = GiftCategory()
         assigned_gift = employee_gift.get_employee_gift(self.id)
         if not assigned_gift:
-            print('assigning gift')
+            current_app.logger.info('no gift is assigned to employee with id {}'.format(self.id))
+            current_app.logger.info('assigning gift')
             suitable_gift_ids = self.get_suitable_gift_ids()
+            current_app.logger.debug('suitable gift ids for employee with id {} are {}'.format(
+                self.id, suitable_gift_ids
+            ))
 
             # find unassigned suitable gifts
             already_assigned_gift_ids = employee_gift.get_all_assigned_gift_ids()
+            current_app.logger.debug('already assigned gift ids are {}'.format(already_assigned_gift_ids))
             unassigned_suitable_gift_ids = suitable_gift_ids - already_assigned_gift_ids
+            current_app.logger.debug('unassigned suitable gift ids are {}'.format(unassigned_suitable_gift_ids))
 
             if unassigned_suitable_gift_ids:
                 assigned_gift_id = next(iter(unassigned_suitable_gift_ids))
+                current_app.logger.debug('assigning gift with id {} to employee with id {}'.format(
+                    assigned_gift_id, self.id))
             else:
-                print('no suitable gift found')
+                current_app.logger.info('no suitable gift found')
                 # find the gift that cannot be given to others
 
                 matched_gift_ids = self.get_all_matching_gift_ids()  # here we get the list of gifts that are
@@ -60,18 +69,27 @@ class Employee(object):
                 unmatched_gift_ids = all_gift_ids - matched_gift_ids
                 unmatched_gift_ids = unmatched_gift_ids - already_assigned_gift_ids
 
+                current_app.logger.debug('unwanted gift ids'.format(unmatched_gift_ids))
+
                 # assign unmatched gift, if exists
                 if unmatched_gift_ids:
                     assigned_gift_id = next(iter(unmatched_gift_ids))
+                    current_app.logger.debug('assigning gift with id {} to employee with id {}'.format(
+                        assigned_gift_id, self.id))
                 else:
                     # find the gift that has less categories, as even the gifts that no one wants are not available
                     available_gift_ids = all_gift_ids - already_assigned_gift_ids
+                    current_app.logger.debug('available gift ids are {}'.format(available_gift_ids))
 
                     if not available_gift_ids:
                         raise ResourceDoesNotExist('no gifts are available')
+                    current_app.logger.info('finding the optimum gift id')
                     assigned_gift_id = gift_category.get_optimum_gift_id(list(available_gift_ids))
+                    current_app.logger.debug('assigning gift with id {} to employee with id {}'.format(
+                        assigned_gift_id, self.id))
 
             assigned_gift = employee_gift.create_employee_gift_record(self.id, assigned_gift_id,
                                                                       'gift assignment succeeded')
+        current_app.logger.info('assigned gift id is {} for employee with id {}'.format(assigned_gift.id, self.id))
 
         return assigned_gift
