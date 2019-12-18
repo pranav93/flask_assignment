259132ef8d0e432892e3432b27b82454d7647aea
pranav <ambhore.pranav@gmail.com>
2019-12-18 13:24:35 +0000

    refactored code

--- functionalities/employees.py
+++ functionalities/employees.py
@@ -1,6 +1,7 @@
 from sqlalchemy import distinct, func, text, not_
 
-from models import db, Gift, EmployeeGift, Employee as EmployeeModel, EmployeeInterest, GiftCategory
+from models import db, Gift, EmployeeGift as EmployeeGiftModel, Employee as EmployeeModel, EmployeeInterest, \
+    GiftCategory
 from utilities.exceptions import ResourceDoesNotExist
 
 
@@ -10,30 +11,42 @@ class Employee(object):
 
     def get_gift(self):
         gift = None
-        employee_gift = EmployeeGift.query.filter_by(employee_id=self.id).first()
+        employee_gift = EmployeeGiftModel.query.filter_by(employee_id=self.id).first()
         if employee_gift and employee_gift.status == 'gift assignment succeeded':
             gift = Gift.query.filter_by(id=employee_gift.gift_id).first()
         return gift
+
+    def get_suitable_gift_ids(self):
+        result = db.session.query(
+            EmployeeModel, EmployeeInterest, GiftCategory, Gift
+        ).join(
+            EmployeeModel, EmployeeModel.id == EmployeeInterest.employee_id
+        ).join(
+            GiftCategory, GiftCategory.category_id == EmployeeInterest.interest_id
+        ).join(
+            Gift, GiftCategory.gift_id == Gift.id
+        ).filter(EmployeeModel.id == self.id).all()
+
+        suitable_gift_ids = {item[3].id for item in result}
+        return suitable_gift_ids
+
+    def get_all_matching_gift_ids(self):
+        result = db.session.query(EmployeeModel, EmployeeInterest, GiftCategory, Gift).join(
+            EmployeeModel, EmployeeModel.id == EmployeeInterest.employee_id).join(
+            GiftCategory, GiftCategory.category_id == EmployeeInterest.interest_id).join(
+            Gift, GiftCategory.gift_id == Gift.id).all()
+
+        return set([item[3].id for item in result])
 
     def assign_gift(self):
         assigned_gift = self.get_gift()
         if not assigned_gift:
             print('assigning gift')
 
-            result = db.session.query(
-                EmployeeModel, EmployeeInterest, GiftCategory, Gift
-            ).join(
-                EmployeeModel, EmployeeModel.id == EmployeeInterest.employee_id
-            ).join(
-                GiftCategory, GiftCategory.category_id==EmployeeInterest.interest_id
-            ).join(
-                Gift, GiftCategory.gift_id==Gift.id
-            ).filter(EmployeeModel.id==self.id).all()
-
-            suitable_gift_ids = {item[3].id for item in result}
+            suitable_gift_ids = self.get_suitable_gift_ids()
 
             # todo: unassigned gifts
-            already_assigned_gift_ids = set([item[0] for item in db.session.query(distinct(EmployeeGift.gift_id)).all()])
+            already_assigned_gift_ids = set([item[0] for item in db.session.query(distinct(EmployeeGiftModel.gift_id)).all()])
 
             unassigned_gift_ids = suitable_gift_ids - already_assigned_gift_ids
 
@@ -42,22 +55,18 @@ class Employee(object):
             else:
                 print('no suitable gift found')
                 # todo: find the gift that cannot be given to others
-                result = db.session.query(EmployeeModel, EmployeeInterest, GiftCategory, Gift).join(
-                    EmployeeModel, EmployeeModel.id == EmployeeInterest.employee_id).join(
-                    GiftCategory, GiftCategory.category_id == EmployeeInterest.interest_id).join(
-                    Gift, GiftCategory.gift_id == Gift.id).all()
-                matched_gift_ids = set([item[3].id for item in result])
+
+                matched_gift_ids = self.get_all_matching_gift_ids()
                 all_gift_ids = set(
                     [item[0] for item in db.session.query(distinct(Gift.id)).all()])
                 unmatched_gift_ids = all_gift_ids - matched_gift_ids
                 unmatched_gift_ids = unmatched_gift_ids - already_assigned_gift_ids
 
-                # todo assign unmatched gift
+                # assign unmatched gift, if exists
                 if unmatched_gift_ids:
                     assigned_gift_id = next(iter(unmatched_gift_ids))
                 else:
-                    # todo: find the gift that has less categories
-                    # todo: find optimum_gift_id
+                    # find the gift that has less categories
                     available_gift_ids = all_gift_ids - already_assigned_gift_ids
 
                     if not available_gift_ids:
@@ -71,7 +80,7 @@ class Employee(object):
                         .first()
                     assigned_gift_id = result[0]
 
-            employee_gift_record = EmployeeGift()
+            employee_gift_record = EmployeeGiftModel()
             employee_gift_record.employee_id = self.id
             employee_gift_record.gift_id = assigned_gift_id
             employee_gift_record.status = 'gift assignment succeeded'
